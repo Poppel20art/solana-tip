cb5ce52b07d4391cee19dc502d7ec85f72abb038
Poppel20art <poppel20.art@gmail.com>
2024-06-27 09:06:24 +0200

    Update const.ts

--- src/app/api/actions/donate/const.ts
+++ src/app/api/actions/donate/const.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
-  "Cuigpd5P2LWTw1KDLWRuyCnA5xFuwUPEZhhZzr54ZL33" // donate wallet
+  "TTXxTsFeQMKPeaAX6n4rZBPhxmXn9JH6f7Mydyo9Qmj" // donate wallet
 );
 
 export const DEFAULT_SOL_AMOUNT: number = 0.01;
